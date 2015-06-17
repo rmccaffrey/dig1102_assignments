036d010798311d0847a0c020d2377179b2579e0d
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2015-06-17 18:45:31 +0000

    playing with arrays

--- 2015JavaScriptCode.js
+++ 2015JavaScriptCode.js
@@ -5,3 +5,15 @@ console.log("It's the JS production code, ya'll!");
 var a = 'foo';
 a += 'bar';
 console.log (a);
+
+//create an array and call parts of it
+
+var students = {
+    'VID':{
+        name:'Rob', start:'Jan 01', program:'Digital Media'
+    }
+}
+
+console.log(students);//returns outmost curly braces
+console.log(students.VID);//returns values within VID
+console.log(students.VID.program);//returns Digital Media
