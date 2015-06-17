c139c0ae3dcceb77f3d9bb7477fceec2502c6e6c
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2015-06-17 19:24:56 +0000

    trying to get the JS asserts to work

--- 2015JavaScriptCode.js
+++ 2015JavaScriptCode.js
@@ -1,6 +1,14 @@
 // get the code to shout its name
 console.log("It's the JS production code, ya'll!");
 
+module.exports = {//production code
+    var add = function (a, b) {
+    return a + b;
+}    
+}
+
+/*
+//random code warmups follow below.
 //create a variable (a), set it to 'foo', then add 'bar'
 var a = 'foo';
 a += 'bar';
@@ -17,3 +25,4 @@ var students = {
 console.log(students);//returns outmost curly braces
 console.log(students.VID);//returns values within VID
 console.log(students.VID.program);//returns Digital Media
+*/
--- 2015JavaScriptTests.js
+++ 2015JavaScriptTests.js
@@ -1,2 +1,5 @@
 var assert = require('assert'), 
     calculator = require("./2015JavaScriptCode.js");
+    
+    //assert a simple test
+    assert(calculator.add(1,1 == 2));
