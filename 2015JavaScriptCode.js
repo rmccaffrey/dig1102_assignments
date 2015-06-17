65b046bc023d1595507976ab3df3e14a127a2f14
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2015-06-17 18:37:56 +0000

    still getting reaquainted

--- 2015JavaScriptCode.js
+++ 2015JavaScriptCode.js
@@ -1,1 +1,7 @@
+// get the code to shout its name
 console.log("It's the JS production code, ya'll!");
+
+//create a variable (a), set it to 'foo', then add 'bar'
+var a = 'foo';
+a += 'bar';
+console.log (a);
